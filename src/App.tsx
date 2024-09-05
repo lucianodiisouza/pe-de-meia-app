b7cabf47df5ffaed71ee9dbae191311a4ad90739
Luciano dii Souza <lucianodiisouza@hotmail.com>
2024-09-04 22:45:17 -0300

    wip: Launch Screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import {TamaguiProvider, Text, View} from 'tamagui'
+import {TamaguiProvider} from 'tamagui'
 import appConfig from "./tamagui/tamagui.config";
 import {SafeAreaView} from "react-native";
-
+import {Home} from "./screens/Home";
 
 export default function App() {
   return (
       <TamaguiProvider config={appConfig}>
-        <SafeAreaView>
-          <View backgroundColor="blue-100" width="$4" h="$10">
-            <Text>Testing Tamagui</Text>
-          </View>
+        <SafeAreaView style={{ backgroundColor: "#F1FFF3", flex: 1}}>
+          <Home />
         </SafeAreaView>
-
       </TamaguiProvider>
   );
 }
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,20 @@
+import {Button, Image, Text, View, YStack} from "tamagui";
+
+import logo from '../../assets/logo.png';
+
+export const Home = () => {
+  return (
+      <YStack ai="center" jc="center" flex={1} gap="$4">
+        <YStack ai="center" jc="center" maxWidth="80%" gap="$2" pb="$4">
+          <Image source={logo} />
+          <Text color="#00D09E" fontWeight="bold" fontSize={32}>Pé de Meia</Text>
+          <Text textAlign="center" fontWeight="100">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod. </Text>
+        </YStack>
+        <YStack gap="$3" ai="center">
+          <Button width="$16" borderRadius="$12" backgroundColor="#00D09E" fontWeight="bold" fontSize="$6" color="#093030">Log In</Button>
+          <Button width="$16" borderRadius="$12" backgroundColor="#DFF7E2" fontWeight="bold" fontSize="$6" color="#093030">Sign Up</Button>
+          <Button backgroundColor="transparent" fontWeight="bold" fontSize={12}>Forgot password?</Button>
+        </YStack>
+      </YStack>
+  )
+}
